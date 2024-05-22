2f7ca3c80f3904fc4f7a1baff29218d8650973b6
Irfan Vigma Taufik <davigmacode@gmail.com>
2024-05-22 21:44:47 +0700

    Drop AnimatedSwitcher support

--- lib/src/widget.dart
+++ lib/src/widget.dart
@@ -22,8 +22,6 @@ class Disclosure extends StatelessWidget {
     this.onClose,
     this.duration = defaultDuration,
     this.curve = defaultCurve,
-    this.transitionBuilder,
-    this.layoutBuilder = Disclosure.defaultLayout,
     this.wrapper = Disclosure.defaultWrapper,
     this.header,
     this.divider,
@@ -49,12 +47,6 @@ class Disclosure extends StatelessWidget {
   /// The curve to apply when animating the parameters of this widget.
   final Curve curve;
 
-  /// A function that wraps a new [content] with an animation that transitions the [content] in when the animation runs in the forward direction and out when the animation runs in the reverse direction. This is only called when a new [content] is set (not for each build), or when a new [transitionBuilder] is set. If a new [transitionBuilder] is set, then the transition is rebuilt for the current content and all previous children using the new [transitionBuilder]. The function must not return null.
-  final DisclosureTransitionBuilder? transitionBuilder;
-
-  /// A function that wraps all of the children that are transitioning out, and the [child] that's transitioning in, with a widget that lays all of them out. This is called every time this widget is built. The function must not return null.
-  final DisclosureLayoutBuilder layoutBuilder;
-
   final DisclosureWrapper wrapper;
 
   /// The header remains persistent, while the collapsible section displays underneath.
@@ -77,11 +69,6 @@ class Disclosure extends StatelessWidget {
   static const defaultDuration = Duration(milliseconds: 200);
   static const defaultCurve = Curves.linear;
 
-  /// The new child is placed in a [Stack] that sizes itself to match the
-  /// largest of the child or a previous child. The children are centered on
-  /// each other.
-  static const defaultLayout = AnimatedSwitcher.defaultLayoutBuilder;
-
   static const defaultWrapper = _defaultWrapper;
   static Widget _defaultWrapper(DisclosureController state, Widget child) {
     return child;
@@ -102,28 +89,17 @@ class Disclosure extends StatelessWidget {
           final hasChild = state.opened || secondary != null;
           final needDivider = hasHeader && hasChild;
 
-          Widget collapsible = transitionBuilder != null
-              ? AnimatedSwitcher(
-                  duration: duration,
-                  switchInCurve: curve,
-                  switchOutCurve: curve,
-                  transitionBuilder: transitionBuilder!,
-                  layoutBuilder: layoutBuilder,
-                  child: state.closed
-                      ? (secondary ?? const SizedBox.shrink())
-                      : child,
-                )
-              : AnimatedCrossFade(
-                  firstChild: secondary ?? Container(),
-                  secondChild: child,
-                  crossFadeState: state.closed
-                      ? CrossFadeState.showFirst
-                      : CrossFadeState.showSecond,
-                  duration: duration,
-                  sizeCurve: curve,
-                  firstCurve: curve,
-                  secondCurve: curve,
-                );
+          Widget collapsible = AnimatedCrossFade(
+            firstChild: secondary ?? Container(),
+            secondChild: child,
+            crossFadeState: state.closed
+                ? CrossFadeState.showFirst
+                : CrossFadeState.showSecond,
+            duration: duration,
+            sizeCurve: curve,
+            firstCurve: curve,
+            secondCurve: curve,
+          );
 
           if (hasHeader) {
             collapsible = Column(
